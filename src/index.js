499486a695cbb48e2f0bfdd754dd393c77bbe71d
poberWong <poberwong@gmail.com>
2019-04-27 23:49:39 +0800

    read input from file with fs and roman converter

--- src/index.js
+++ src/index.js
@@ -1,46 +1,58 @@
-const inputStr = `glob is I
-prok is V
-pish is X
-tegj is L
-glob glob Silver is 34 Credits
-glob prok Gold is 57800 Credits
-pish pish Iron is 3910 Credits
-how much is pish tegj glob glob ?
-how many Credits is glob prok Silver ?
-how many Credits is glob prok Gold ?
-how many Credits is glob prok Iron ?`
+const fs = require('fs')
 
-const ROMAN_MAP = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-}
+const ROMAN_MAP = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000, CM: 900, CD: 400, XC: 90, XL: 40, IX: 9, IV: 4}
+const ROMAN_ORDER = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+
 const ERROR_TYPE = {
     ERROR_QUESTION: 'bad question',
     INVALID_SYMBOLS: 'symbols can\'t convert into arabic',
+    INVALID_ROMAN: 'roman string can\'t be converted to arabic',
     UNKNOWN_INPUT: 'input can\'t be parsed'
 }
 
 let wordMap = {}, metalMap = {}
 
-function symbolsToArabic (symbols) {
-    const romans = symbols.map(word => {
+/**
+ * 
+ * @param {string} romans 
+ */
+function romansToArabic (romans) {
+    if(!/^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/.test(romans)) {
+        throw new Error(ERROR_TYPE.INVALID_ROMAN)
+    }
+    let res = 0
+    ROMAN_ORDER.forEach(key => {
+        while(romans.startsWith(key)) {
+            romans = romans.replace(key, '')
+            res += ROMAN_MAP[key]
+        }
+    })
+    return res
+}
+
+/**
+ * 
+ * @param {array} symbols 
+ */
+function symbolsToRoman (symbols) {
+    return symbols.map(word => {
         const roman = wordMap[word]
         if(roman) {
             return roman
         } else {
             throw new Error(ERROR_TYPE.INVALID_SYMBOLS)
         }
-    })
+    }).join('')
+}
+
+function symbolsToArabic (symbols) {
+    return romansToArabic(symbolsToRoman(symbols))
 }
 
 function parseWord (line) {
     const words = line.split(' ')
     wordMap[words[0]] = words.pop()
+    return wordMap
 }
 
 function caculateMetal (line) {
@@ -49,6 +61,7 @@ function caculateMetal (line) {
     const metal = frontIs.split(' ').pop()
     const symbols = frontIs.split(' ').slice(0, -1)
     metalMap[metal] = creditCount / symbolsToArabic(symbols)
+    return metalMap
 }
 
 /**
@@ -56,7 +69,7 @@ function caculateMetal (line) {
  * @param line 
  */
 function answerNumber (line) {
-    const behindIs = line.split(' is ')[1]
+    const behindIs = line.split(' is ')[1].replace(' ?', '')
     const number = symbolsToArabic(behindIs.split(' '))
     console.log(`${behindIs} is ${number}`)
 }
@@ -66,10 +79,11 @@ function answerNumber (line) {
  * @param line 
  */
 function answerCredits (line) {
-    const behindIsArr = line.split(' is ')[1].split(' ')
-    const metal = behindIsArr[behindIsArr[behindIsArr.length - 1]]
+    const behindIs = line.split(' is ')[1].replace(' ?', '')
+    const behindIsArr = behindIs.split(' ')
+    const metal = behindIsArr[behindIsArr.length - 1]
     const number = symbolsToArabic(behindIsArr.slice(0, -1))
-    console.log(`${behindIsArr} is ${number} Credits`)
+    console.log(`${behindIs} is ${number * metalMap[metal]} Credits`)
 }
 
 function answerQuestion (line) {
@@ -94,11 +108,14 @@ function parseLine (line) {
     }
 }
 
-function parseInput (str) {
-    const lines = str.split('\n')
-    lines.forEach(parseLine)
-}
-
-
-
-
+fs.readFile('src/data.txt', (err, data) => {
+    if(err) {
+        console.log('err: ', err)
+    } else {
+        try {
+            data.toString().split('\n').forEach(parseLine)
+        } catch (err) {
+            console.log('I have no idea what you are talking about')
+        }
+    }
+})
