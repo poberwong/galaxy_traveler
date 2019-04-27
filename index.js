ac7e6286424aca5f2d147a1f0c87542bea4f3d8e
poberWong <poberwong@gmail.com>
2019-04-28 05:43:34 +0800

    add file path as input

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const commander = require('commander')
 const SymbolConverter = require('./src/SymbolConverter')
 const Parser = require('./src/Parser')
 const { ROMAN_MAP, ROMAN_ORDER } = require('./src/helpers/Constant')
 
 const symbolConverter = new SymbolConverter(ROMAN_MAP, ROMAN_ORDER)
 const parser = new Parser(symbolConverter)
-parser.run('src/data.txt')
+
+commander.option('-i, --inputFile [type]', 'file path').parse(process.argv)
+
+const path = (commander.inputFile && commander.inputFile === String) ? commander.inputFile : 'src/data.txt'
+parser.run(path)
